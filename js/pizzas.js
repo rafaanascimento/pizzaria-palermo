90fa7242ce0640577d5c18281ea304c5ac6a07b6
Rafael Nascimento <rafaeln@unigranrio.br>
2020-06-19 22:34:32 -0300

    versão 1.0

---
+++ js/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'imagens/pizza.png', price:20.19, sizes:['200g', '530g', '860g'], description:'Mussarela, requeijão e parmesão ralado'},
+    {id:2, name:'Calabresa', img:'imagens/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Mussarela, calabresa e cebola.'},
+    {id:3, name:'Quatro Queijos', img:'imagens/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Mussarela, requeijão, gorgonzola e parmesão ralado.'},
+    {id:4, name:'Americana', img:'imagens/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Mussarela, pepperoni, champignon, cebola e pimentão verde.'},
+    {id:5, name:'Napolitana', img:'imagens/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Mussarela, tomate e parmesão ralado.'},
+    {id:6, name:'Toscana', img:'imagens/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Mussarela, calabresa, requeijão, azeitona preta, cebola, tomate e manjericão.'},
+    {id:7, name:'Chocolate', img:'imagens/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Chocolate, confetti e brigadeiro'}
+];
